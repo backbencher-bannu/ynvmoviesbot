cdf01756eef553309e6a3c47aeb1d3c1d4fb22ef
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-05-11 15:01:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,10 +90,10 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/How_to_Download_7x/30
 # ============================
 # Link Shortener Configuration
 # ============================
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'inshorturl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_Download_7x/30')  # Tutorial video link for opening shortlink website
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'avhlink.site')
+SHORTLINK_API = environ.get('SHORTLINK_API', '858bed32cb3a9e68de45894f9fd76a86e655b9b0')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_Download_7/30')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
 # ============================
