0f0edc487b2e4d9ff7a76035ce8f6c9ca78c2c1b
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-05-11 10:48:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harishkumargorinta:P6ak9kZKvx33Jx3@ynvmovies.p8afuzn.mongodb.net/?retryWrites=true&w=majority&appName=ynvmovies")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://harishkumargorinta:P6ak9kZKvx33Jx3@ynvmovies.p8afuzn.mongodb.net/?retryWrites=true&w=majority&appName=ynvmovies")
 DATABASE_NAME = environ.get('DATABASE_NAME', "yato")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
