b7aafa6646e6b7d237b4e24048e0b0c6048dff5f
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-05-12 13:38:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,11 +44,11 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002290275432'))  # Log channel i
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002334132732'))  # Bin channel id (make sure bot is admin)
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002226636138'))  # Notification of those who verify will be sent to your channel
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002290275432'))  # Premium logs channel id
-auth_channel = environ.get('AUTH_CHANNEL', '-1002593931448')  # Channel/Group ID for force sub (make sure bot is admin)
+auth_channel = environ.get('AUTH_CHANNEL', '-1002479054069')  # Channel/Group ID for force sub (make sure bot is admin)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002334132732').split()]
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002229214716')  # Support group id (make sure bot is admin)
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002204842444')  # Request channel id (make sure bot is admin)
-AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '-1002593931448').split()]
+AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '-1002479054069').split()]
 MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002479054069').split() if re.match(r'^-?\d+$', channel_id)]  # Channel for force sub (make sure bot is admin)
 
 
